a6ab911e290c49b2b5c28b0876e0740bb93695c3
Blue <1347917812@qq.com>
2018-06-01 09:29:38 +0800

    导航栏

--- CFBaseProject/CFBaseProject/ViewController.m
+++ CFBaseProject/CFBaseProject/ViewController.m
@@ -9,8 +9,7 @@
 #import "ViewController.h"
 #import "UINavigationController+QMUI.h"
 #import "UIViewController+HBD.h"
-//#import "HBDNavigationController.h"
-
+#import "YPGradientDemoViewController.h"
 
 @interface ViewController ()<UINavigationControllerBackButtonHandlerProtocol>
 
@@ -30,8 +29,27 @@
 }
 - (void)btnClick {
 //    ViewController *vc = [ViewController new];
-    UIViewController *vc = [self createDemoViewController];
+//    UIViewController *vc = [self createDemoViewController];
+    UIViewController *vc = [YPGradientDemoViewController new];
     [self.navigationController pushViewController:vc animated:YES];
+}
+
+- (UIViewController *)createDemoViewController {
+    ViewController *vc = [ViewController new];
+    
+    vc.hbd_barShadowHidden = NO;
+    vc.hbd_barHidden = NO;
+    vc.hbd_barStyle = UIBarStyleDefault;
+    UIColor *color = @[
+                       [UIColor colorWithRed:47/255.0 green:247/255.0 blue:247/255.0 alpha:0.8],
+                       [UIColor colorWithRed:28/255.0 green:28/255.0 blue:28/255.0 alpha:0.729],
+                       [UIColor.redColor colorWithAlphaComponent:0.7],
+                       [UIColor.greenColor colorWithAlphaComponent:0.7],
+                       [UIColor.blueColor colorWithAlphaComponent:0.8]
+                       ][arc4random()%5];
+    
+    vc.hbd_barTintColor = color;
+    return vc;
 }
 
 #pragma mark - UINavigationControllerBackButtonHandlerProtocol
@@ -43,17 +61,17 @@
 - (BOOL)canPopViewController {
     // 这里不要做一些费时的操作，否则可能会卡顿。
     if (1) {
-        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"是否返回？" message:@"返回后输入框的数据将不会自动保存" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"是否返回？" message:@"拦截系统返回" preferredStyle:UIAlertControllerStyleAlert];
         UIAlertAction *backActioin = [UIAlertAction actionWithTitle:@"返回" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
             [self.navigationController popViewControllerAnimated:YES];
         }];
-        UIAlertAction *continueAction = [UIAlertAction actionWithTitle:@"继续编辑" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-//            [self.textView becomeFirstResponder];
+        UIAlertAction *continueAction = [UIAlertAction actionWithTitle:@"停留" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+
         }];
         [alertController addAction:backActioin];
         [alertController addAction:continueAction];
         [self presentViewController:alertController animated:YES completion:nil];
-//        [alertController showWithAnimated:YES];
+
         return NO;
     } else {
         return YES;
@@ -61,30 +79,5 @@
 }
 
 
-
-
-
-
-
-
-
-
-- (UIViewController *)createDemoViewController {
-    ViewController *vc = [ViewController new];
-    
-    vc.hbd_barShadowHidden = NO;
-    vc.hbd_barHidden = NO;
-    vc.hbd_barStyle = UIBarStyleDefault;
-    UIColor *color = @[
-                       [UIColor colorWithRed:247/255.0 green:247/255.0 blue:247/255.0 alpha:0.8],
-                       [UIColor colorWithRed:28/255.0 green:28/255.0 blue:28/255.0 alpha:0.729],
-                       [UIColor.redColor colorWithAlphaComponent:0.7],
-                       [UIColor.greenColor colorWithAlphaComponent:0.7],
-                       [UIColor.blueColor colorWithAlphaComponent:0.8]
-                       ][arc4random()%5];
-    
-    vc.hbd_barTintColor = color;
-    return vc;
-}
 
 @end
